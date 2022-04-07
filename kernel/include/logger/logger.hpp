f3824e203e51e21cb8f1e1b7f21f66a2ecb88855
Yoav Strugo <yoav@strugo.net>
2022-04-07 13:21:43 -0700

    direct print

--- kernel/include/logger/logger.hpp
+++ kernel/include/logger/logger.hpp
@@ -6,6 +6,8 @@
 
 void loggerInitiallize(stivale2_struct *stivale2Struct);
 
+void loggerPrintDirect(const char *msg, va_list valist);
+
 void loggerPrint(const char *msg, ...);
 
 void loggerPrintln(const char *msg, ...);
