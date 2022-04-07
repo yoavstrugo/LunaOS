c09039bb388f1106a5a6b14ac6c0ea81716b2eb1
yoavstrugo <58818812+yoavstrugo@users.noreply.github.com>
2022-04-07 16:52:51 +0300

    Delete kernel/lib directory

--- kernel/lib/Renderer.d
+++
@@ -1,2 +0,0 @@
-lib/Renderer.o: src/graphics/Renderer.cpp include/Renderer.hpp \
- include/stivale2.h include/PSF.hpp include/strings.hpp
--- kernel/lib/kernel.d
+++
@@ -1,4 +0,0 @@
-lib/kernel.o: src/kernel.cpp include/stivale2.h include/strings.hpp \
- include/Renderer.hpp include/PSF.hpp \
- include/memory/PhysicalMemoryAllocator.hpp include/Bitmap.hpp \
- include/types.hpp include/constants.hpp include/memory/Paging.hpp
