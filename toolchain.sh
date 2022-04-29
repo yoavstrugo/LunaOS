e388385f55ffbf0d3817be4bcf564a6b9bfa16af
Yoav <yoav@strugo.net>
2022-04-30 00:30:44 +0300

    installing all the required packages

---
+++ toolchain.sh
@@ -0,0 +1,30 @@
+apt-get --yes install make
+
+# Install QEMU
+apt-get --yes install qemu-system-x86
+
+# Install OVMF
+apt-get --yes install ovmf
+
+# Instell mtools
+apt-get --yes install mtools
+
+# Install binutils
+apt-get --yes install binutils
+
+# Install NASM
+apt-get --yes install nasm
+
+# Install GCC
+apt-get --yes install gcc
+
+# Install G++
+apt-get --yes install g++
+
+# Install GDB
+apt-get --yes install gdb
+
+# Install xorriso
+apt-get --yes install xorriso
+
+echo "Toolchain installation finished!"
