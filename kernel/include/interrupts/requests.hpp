e175bd6f08f64762bcc2e2a1ecdb2a67db797e2f
Yoav <yoav@strugo.net>
2022-05-10 01:04:09 +0300

    keyboard press

--- kernel/include/interrupts/requests.hpp
+++ kernel/include/interrupts/requests.hpp
@@ -1,1 +1,4 @@
 #pragma once
+
+struct interrupt_frame;
+__attribute__((interrupt)) void requestKeyboardInt(interrupt_frame *frame);
--- kernel/src/interrupts/requests.cpp
+++ kernel/src/interrupts/requests.cpp
@@ -1,1 +1,11 @@
 #include <interrupts/requests.hpp>
+
+#include <logger/logger.hpp>
+#include <interrupts/pic.hpp>
+#include <io.hpp>
+
+void requestKeyboardInt(interrupt_frame *frame) {
+    logInfon("Pressed!");
+    ioInByte(0x60);
+    picSendEOI(0x21);
+}
