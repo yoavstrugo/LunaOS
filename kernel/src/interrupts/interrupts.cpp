a2cc3f9d1476a5669067c5e74b59c047e0e64382
Yoav <yoav@strugo.net>
2022-05-10 01:04:58 +0300

    added some interrupts and fixed sti

--- kernel/src/interrupts/interrupts.cpp
+++ kernel/src/interrupts/interrupts.cpp
@@ -3,16 +3,18 @@
 #include <interrupts/idt.hpp>
 #include <logger/logger.hpp>
 #include <interrupts/exceptions.hpp>
+#include <interrupts/requests.hpp>
 #include <kernel.hpp>
 
 void interruptsInitiallize() {
     idtInitiallize();
     idtLoad();
     interruptsInstallRoutines();
+    interruptsEnable();
 }
 
 void interruptsEnable() {
-    asm ("cli");
+    asm ("sti");
 }
 
 void interruptsDisable() {
@@ -20,5 +22,8 @@ void interruptsDisable() {
 }
 
 void interruptsInstallRoutines() {
+    idtCreateEntry(0x08, (uint64_t)exceptionDoubleFault, 0x08, 0x00, K_IDT_TA_INTERRUPT);
+    idtCreateEntry(0x0D, (uint64_t)exceptionGPFault, 0x08, 0x00, K_IDT_TA_INTERRUPT);
     idtCreateEntry(0x0E, (uint64_t)exceptionPageFault, 0x08, 0x00, K_IDT_TA_INTERRUPT);
+    idtCreateEntry(0x21, (uint64_t)requestKeyboardInt, 0x08, 0x00, K_IDT_TA_INTERRUPT);
 }
