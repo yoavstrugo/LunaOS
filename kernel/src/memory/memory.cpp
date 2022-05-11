90002e607e0e75e6c17b60f03ba974a6ba13ffb7
Yoav <yoav@strugo.net>
2022-05-12 01:01:23 +0300

    heap initiallization

--- kernel/src/memory/memory.cpp
+++ kernel/src/memory/memory.cpp
@@ -3,14 +3,15 @@
 #include <memory/paging.hpp>
 #include <kernel.hpp>
 #include <stivale2/stivale2_tools.hpp>
+#include <memory/heap.hpp>
 
 BitmapAllocator memoryPhysicalAllocator;
 
-void memoryInitiallize(stivale2_struct *stivale2Info) {
+void memoryInitialize(stivale2_struct *stivale2Info) {
     stivale2_struct_tag_memmap *memMap = (stivale2_struct_tag_memmap *)
                                             stivale2_get_tag(stivale2Info, STIVALE2_STRUCT_TAG_MEMMAP_ID);
     if (memMap == NULL) kernelPanic("Memory map was not found!");
-    memoryPhysicalAllocator.initiallize(memMap);
+    memoryPhysicalAllocator.initialize(memMap);
 
     stivale2_struct_tag_hhdm *hhdm = (stivale2_struct_tag_hhdm *)
                                         stivale2_get_tag(stivale2Info, STIVALE2_STRUCT_TAG_HHDM_ID);
@@ -19,5 +20,8 @@ void memoryInitiallize(stivale2_struct *stivale2Info) {
         = (stivale2_struct_tag_kernel_base_address *)  
             stivale2_get_tag(stivale2Info, STIVALE2_STRUCT_TAG_KERNEL_BASE_ADDRESS_ID);
 
-    pagingInitiallize(kernelBase->physical_base_address, hhdm->addr);
+    pagingInitialize(kernelBase->physical_base_address, hhdm->addr);
+
+    // Initialize the kernel's heap
+    heapInitialize(K_HEAP_DEFAULT_START, K_HEAP_DEFAULT_END);
 }
