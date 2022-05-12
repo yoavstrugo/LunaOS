cb14f47fdeb19f5d3506da9c88b0e08402ce5174
Yoav <yoav@strugo.net>
2022-05-13 00:40:45 +0300

    added virtual ranges allocator support

--- kernel/include/memory/memory.hpp
+++ kernel/include/memory/memory.hpp
@@ -2,11 +2,16 @@
 
 #include <stivale2.h>
 #include <memory/bitmap_allocator.hpp>
+#include <memory/virtual_address_range_allocator.hpp>
 
-#define K_HEAP_DEFAULT_START    0xFFFF800000100000
-#define K_HEAP_DEFAULT_END      0xFFFF800000200000
-#define K_HEAP_MAX_EXPANSION    0xFFFF800100000000
+#define K_CONST_HEAP_DEFAULT_START              0xFFFF800000100000
+#define K_CONST_HEAP_DEFAULT_END                0xFFFF800000200000
+#define K_CONST_HEAP_MAX_EXPANSION              0xFFFF800050100000
+
+#define K_CONST_KERNEL_VIRTUAL_RANGES_START     0xFFFF800050100000
+#define K_CONST_KERNEL_VIRTUAL_RANGES_END       0xFFFF8000A0100000
 
 extern BitmapAllocator memoryPhysicalAllocator;
+extern k_virtual_address_range_allocator virtualAddressRangeAllocator;
 
 void memoryInitialize(stivale2_struct *stivale2Info);
--- kernel/src/memory/memory.cpp
+++ kernel/src/memory/memory.cpp
@@ -6,6 +6,7 @@
 #include <memory/heap.hpp>
 
 BitmapAllocator memoryPhysicalAllocator;
+k_virtual_address_range_allocator virtualAddressRangeAllocator;
 
 void memoryInitialize(stivale2_struct *stivale2Info) {
     stivale2_struct_tag_memmap *memMap = (stivale2_struct_tag_memmap *)
@@ -20,8 +21,12 @@ void memoryInitialize(stivale2_struct *stivale2Info) {
         = (stivale2_struct_tag_kernel_base_address *)  
             stivale2_get_tag(stivale2Info, STIVALE2_STRUCT_TAG_KERNEL_BASE_ADDRESS_ID);
 
+    // Initialize paging
     pagingInitialize(kernelBase->physical_base_address, hhdm->addr);
 
     // Initialize the kernel's heap
-    heapInitialize(K_HEAP_DEFAULT_START, K_HEAP_DEFAULT_END);
+    heapInitialize(K_CONST_HEAP_DEFAULT_START, K_CONST_HEAP_DEFAULT_END);
+
+    // Intialize the kernel's virtual address ranges allocator
+    virtualAddressRangeAllocator.addRange(K_CONST_KERNEL_VIRTUAL_RANGES_START, K_CONST_KERNEL_VIRTUAL_RANGES_END);
 }
