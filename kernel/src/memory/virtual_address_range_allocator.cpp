47091937bc0098aa8e87e179baafd59bd184e4ee
Yoav <yoav@strugo.net>
2022-05-13 14:23:48 +0300

    fixed some issues and added debug info

--- kernel/src/memory/virtual_address_range_allocator.cpp
+++ kernel/src/memory/virtual_address_range_allocator.cpp
@@ -5,11 +5,13 @@
 #include <logger/logger.hpp>
 #include <stddef.h>
 
-k_virtual_address_range_allocator::k_virtual_address_range_allocator() {
+k_virtual_address_range_allocator::k_virtual_address_range_allocator()
+{
     this->head = 0;
 }
 
-void k_virtual_address_range_allocator::addRange(virtual_address_t start, virtual_address_t end) {
+void k_virtual_address_range_allocator::addRange(virtual_address_t start, virtual_address_t end)
+{
     // TODO: check that start and end are page aligned
     k_address_range_header *addressHeader = new k_address_range_header();
 
@@ -17,54 +19,64 @@ void k_virtual_address_range_allocator::addRange(virtual_address_t start, virtua
     addressHeader->pages = (end - start) / PAGE_SIZE;
     addressHeader->used = false;
 
-    if(!this->head) {
-        this->head = addressHeader;
-        return;
-    } 
-
-    // We need to sort the list, so we may be able to merge ranges
-    k_address_range_header *found = 0;
-    k_address_range_header *curr = this->head;
-
-    while (curr) {
-        if (addressHeader->base < curr->base)
-            found = curr;
-        curr = curr->next;
-    }
-
-    if (found) {
-        // Put it in between the lower range and higher range
-        addressHeader->next = found->next;
-        found->next = addressHeader;
-    } else {
-        // It's the smallest in the list, it shall be first
-        addressHeader->next = this->head->next;
+    if (!this->head)
+    {
         this->head = addressHeader;
     }
+    else
+    {
+        // We need to sort the list, so we may be able to merge ranges
+        k_address_range_header *found = 0;
+        k_address_range_header *curr = this->head;
 
+        while (curr)
+        {
+            if (addressHeader->base < curr->base)
+                found = curr;
+            curr = curr->next;
+        }
+
+        if (found)
+        {
+            // Put it in between the lower range and higher range
+            addressHeader->next = found->next;
+            found->next = addressHeader;
+        }
+        else
+        {
+            // It's the smallest in the list, it shall be first
+            addressHeader->next = this->head->next;
+            this->head = addressHeader;
+        }
+    }
+
+    logDebugn("%! Added range 0x%64x-0x%64x (%d pages)", "[VAddr Allocator]", start, end, addressHeader->pages);
     this->mergeAll();
 }
 
-virtual_address_t k_virtual_address_range_allocator::allocateRange(uint64_t pages) {
+virtual_address_t k_virtual_address_range_allocator::allocateRange(uint64_t pages)
+{
     k_address_range_header *range = this->head;
-    
+
     // Find a range to fit the allocation
-    while (range) {
+    while (range)
+    {
         if (!range->used && range->pages >= pages)
             break;
 
         range = range->next;
     }
 
-    if (!range) {
-        logDebugn("%! Couldn't find any virtual ranges to fit allocation,", "[Virtual Address Range Allocator]");
+    if (!range)
+    {
+        logWarnn("%! Couldn't find any virtual ranges to fit allocation of %d pages.", "[VAddr Allocator]", pages);
         return NULL;
     }
 
     range->used = true;
-    range->pages = pages;
 
-    if (range->pages > pages) {
+    if (range->pages > pages)
+    {
         k_address_range_header *split = new k_address_range_header();
         split->used = false;
         split->next = range->next;
@@ -74,52 +86,64 @@ virtual_address_t k_virtual_address_range_allocator::allocateRange(uint64_t page
         range->next = split;
     }
 
+    range->pages = pages;
+
+    logDebugn("%! Allocated %d pages, from base 0x%64x.", "[VAddr Allocator]", pages, range->base);
     return range->base;
 }
 
-void k_virtual_address_range_allocator::mergeAll() {
+void k_virtual_address_range_allocator::mergeAll()
+{
     k_address_range_header *range = this->head;
 
-    while (range && range->next) {
-        if (!range->used && 
-            !range->next->used && 
-            (range->base + range->pages * PAGE_SIZE == range->next->base)) 
+    while (range && range->next)
+    {
+        if (!range->used &&
+            !range->next->used &&
+            (range->base + range->pages * PAGE_SIZE == range->next->base))
         {
             // Ranges aren't in use, and they continue each other
             // therefore we can merge them
             range->pages += range->next->pages;
-            range->next = range->next->next;
+            k_address_range_header *followingRange = range->next;
+            range->next = followingRange->next;
 
-            logDebugn("%! Successfully merged ranges from %d pages to %d pages.", 
-                "[Virtual Address Range Allocator]", 
-                range->pages - range->next->pages, 
-                range->pages);
+            logDebugn("%! Successfully merged ranges from %d pages to %d pages.",
+                      "[VAddr Allocator]",
+                      range->pages - followingRange->pages,
+                      range->pages);
 
             // no longer need to next range header
-            delete range->next;
+            delete followingRange;
         }
-
-        range = range->next;
+        else
+        {
+            range = range->next;
+        }
     }
 }
 
-void k_virtual_address_range_allocator::freeRange(virtual_address_t base) {
+void k_virtual_address_range_allocator::freeRange(virtual_address_t base)
+{
     // find the range to free
     k_address_range_header *range = this->head;
 
-    while(range && range->base != base) 
+    while (range && range->base != base)
         range = range->next;
 
-    if (!range) {
-        logDebugn("%! Tried to free a non-existing range, base 0x%64x.", "[Virtual Address Range Allocator]", base);
+    if (!range)
+    {
+        logWarnn("%! Tried to free a non-existing range, base 0x%64x.", "[VAddr Allocator]", base);
         return;
-    } 
-    
-    if (!range->used) {
-        logDebugn("%! Tried to free a free range, base 0x%64x.", "[Virtual Address Range Allocator]", base);
+    }
+
+    if (!range->used)
+    {
+        logWarnn("%! Tried to free a free range, base 0x%64x.", "[VAddr Allocator]", base);
         return;
     }
 
     range->used = false;
+    logDebugn("%! Successfully freed range of size %d pages, base 0x%64x", "[VAddr Allocator]", range->pages, range->base);
     this->mergeAll();
 }
