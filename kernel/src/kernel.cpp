12f00a9d9c860f1be39ee681dd24f85ca935ae4c
Yoav <yoav@strugo.net>
2022-05-14 23:30:51 +0300

    testing acpi and apic

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -12,16 +12,19 @@
 
 #include <strings.hpp>
 
-#include <gdt/gdt.hpp>
 #include <interrupts/interrupts.hpp>
 #include <interrupts/pic.hpp>
+#include <interrupts/lapic.hpp>
+#include <interrupts/ioapic.hpp>
 
+#include <gdt/gdt.hpp>
 #include <memory/memory.hpp>
 #include <memory/buddy_allocator.hpp>
 #include <memory/heap.hpp>
 
 #include <system/acpi/rsdp.hpp>
 #include <system/acpi/acpi.hpp>
+#include <system/acpi/madt.hpp>
 
 // We need to tell the stivale bootloader where we want our stack to be.
 // We are going to allocate our stack as an array in .bss.
@@ -30,7 +33,6 @@ static uint8_t stack[0x100000];
 // stivale2 uses a linked list of tags for both communicating TO the
 // bootloader, or receiving info FROM it. More information about these tags
 // is found in the stivale2 specification.
-
 
 // We are now going to define a framebuffer header tag.
 // This tag tells the bootloader that we want a graphical framebuffer instead
@@ -42,22 +44,20 @@ static struct stivale2_header_tag_framebuffer framebuffer_hdr_tag = {
         .identifier = STIVALE2_HEADER_TAG_FRAMEBUFFER_ID,
         // Instead of 0, we now point to the previous header tag. The order in
         // which header tags are linked does not matter.
-        .next = 0//(uint64_t)&terminal_hdr_tag
+        .next = 0 //(uint64_t)&terminal_hdr_tag
     },
     // We set all the framebuffer specifics to 0 as we want the bootloader
     // to pick the best it can.
-    .framebuffer_width  = 0,
+    .framebuffer_width = 0,
     .framebuffer_height = 0,
-    .framebuffer_bpp    = 0,
-    .unused = 0
-};
+    .framebuffer_bpp = 0,
+    .unused = 0};
 
 // The stivale2 specification says we need to define a "header structure".
 // This structure needs to reside in the .stivale2hdr ELF section in order
 // for the bootloader to find it. We use this __attribute__ directive to
 // tell the compiler to put the following structure in said section.
-__attribute__((section(".stivale2hdr"), used))
-static struct stivale2_header stivale_hdr = {
+__attribute__((section(".stivale2hdr"), used)) static struct stivale2_header stivale_hdr = {
     // The entry_point member is used to specify an alternative entry
     // point that the bootloader should jump to instead of the executable's
     // ELF entry point. We do not care about that so we leave it zeroed.
@@ -79,18 +79,19 @@ static struct stivale2_header stivale_hdr = {
     .flags = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4),
     // This header structure is the root of the linked list of header tags and
     // points to the first one in the linked list.
-    .tags = (uintptr_t)&framebuffer_hdr_tag
-};
-
+    .tags = (uintptr_t)&framebuffer_hdr_tag};
 
 // The following will be our kernel's entry point.
-extern "C" void kernelMain(stivale2_struct *stivale2Struct) {
+extern "C" void kernelMain(stivale2_struct *stivale2Struct)
+{
     kernelInitialize(stivale2Struct);
-    for(;;);
+    for (;;)
+        ;
     // kernelHalt();
 }
 
-void kernelInitialize(stivale2_struct *stivaleInfo) {
+void kernelInitialize(stivale2_struct *stivaleInfo)
+{
     loggerInitialize(stivaleInfo);
     logDebugn("Logger has been initialized.");
 
@@ -100,46 +101,33 @@ void kernelInitialize(stivale2_struct *stivaleInfo) {
 
     interruptsInitialize();
     picRemapIRQs();
-    picUnsetMask(1);
+    // picUnsetMask(1);
 
-    // asm("int 0x21");
-    
     memoryInitialize(stivaleInfo);
-    logDebugn("Memory has been initialized.");
+    heapAllocate(1025);
+    heapAllocate(511);
+    heapAllocate(1024 * 1024 + 1000);
 
-    stivale2_struct_tag_rsdp *rsdpTag = (stivale2_struct_tag_rsdp *)stivale2_get_tag(stivaleInfo, STIVALE2_STRUCT_TAG_RSDP_ID);
-    if (rsdpTag == NULL)
-        kernelPanic("%! couldn't be found!", "RSDP");
-    
-    if(rsdpValidate((k_rsdp_descriptor *)rsdpTag->rsdp)) {
-        k_rsdp_descriptor_20 * rsdp = (k_rsdp_descriptor_20 *)rsdpTag->rsdp;
-        logDebugn("%! RSDP is valid. R/XSDT is in 0x%64x", "[ACPI]", rsdp->xsdtAddress);
-        if(acpiValidateHeader(&((k_xsdt *)(rsdp->xsdtAddress))->h)) {
-            logDebugn("%! SDT header checks up!", "ACPI");
-        }
+
+    acpiInitialize(stivaleInfo);
+    k_acpi_sdt_hdr *madtHeader = acpiGetEntryWithSignature("APIC");
+    if (!madtHeader)
+    {
+        logWarnn("%! Couldn't find MADT", "[MADT]");
+    }
+    else
+    {
+        madtParse(madtHeader);
     }
 
-    heapAllocate(0x1000);
-    heapAllocate(0x10000);
-    heapAllocate(0x10000);
-    heapAllocate(0x10000);
-    heapAllocate(0x10000);
-    heapAllocate(0x100000);
+    lapicInitialize();
 
-
-    // k_buddyallocator buddyAllocator = k_buddyallocator(0x100000, 0x10000);
-    // virtual_address_t ptr = buddyAllocator.allocate(0x1000);
-    // virtual_address_t ptr1 = buddyAllocator.allocate(100);
-    // virtual_address_t ptr2 = buddyAllocator.allocate(510);
-    // virtual_address_t ptr3 = buddyAllocator.allocate(1002);
-
-
-
-    // buddyAllocator.deallocate(ptr1);
-    // buddyAllocator.deallocate(ptr3);
+    ioapicCreateISARedirection(1, 1, 0); 
+    lapicStartTimer();
 }
 
-void kernelPanic(const char *msg, ...) {
+void kernelPanic(const char *msg, ...)
+{
     logInfon("%!", "-+== KERNEL PANIC ==+-");
     va_list valist;
     va_start(valist, msg);
@@ -148,17 +136,20 @@ void kernelPanic(const char *msg, ...) {
 
     interruptsDisable();
 
-    for (;;) {
-        asm ("hlt");
+    for (;;)
+    {
+        asm("hlt");
     }
 }
 
-void kernelHalt() {
-    // TODO: disable interrupts
+void kernelHalt()
+{
+    interruptsDisable();
 
     logInfon("Kernel execution has been finished.");
 
-    for (;;) {
-        asm ("hlt");
+    for (;;)
+    {
+        asm("hlt");
     }
 }
