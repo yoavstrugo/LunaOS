ed0cf200147ed1cc224bd356042d93d62759d0cd
Yoav <yoav@strugo.net>
2022-05-14 23:31:04 +0300

    adding timer interrupt

--- kernel/src/interrupts/interrupts.cpp
+++ kernel/src/interrupts/interrupts.cpp
@@ -25,5 +25,6 @@ void interruptsInstallRoutines() {
     idtCreateEntry(0x08, (uint64_t)exceptionDoubleFault, 0x08, 0x00, K_IDT_TA_INTERRUPT);
     idtCreateEntry(0x0D, (uint64_t)exceptionGPFault, 0x08, 0x00, K_IDT_TA_INTERRUPT);
     idtCreateEntry(0x0E, (uint64_t)exceptionPageFault, 0x08, 0x00, K_IDT_TA_INTERRUPT);
+    idtCreateEntry(0x20, (uint64_t)requestTimer, 0x08, 0x00, K_IDT_TA_INTERRUPT);
     idtCreateEntry(0x21, (uint64_t)requestKeyboardInt, 0x08, 0x00, K_IDT_TA_INTERRUPT);
 }
