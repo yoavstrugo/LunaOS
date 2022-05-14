0abb600e3a194455f6877409beb77961d59057b0
Yoav <yoav@strugo.net>
2022-05-14 14:31:09 +0300

    formatted

--- kernel/src/memory/heap.cpp
+++ kernel/src/memory/heap.cpp
@@ -12,7 +12,8 @@ bool heapInitialized = false;
 k_freelist_allocator heapAllocator;
 uint64_t heapUsed;
 
-void heapInitialize(virtual_address_t start, virtual_address_t end) {
+void heapInitialize(virtual_address_t start, virtual_address_t end)
+{
     heapAllocator.init(start, end);
     heapStart = start;
     heapEnd = end;
@@ -22,41 +23,50 @@ void heapInitialize(virtual_address_t start, virtual_address_t end) {
 
     // Lock the pages in the heap
     uint64_t pageCount = ((uint64_t)(end - start)) / PAGE_SIZE + 1;
-    for (uint64_t page = 0; page < pageCount; page++) {
+    for (uint64_t page = 0; page < pageCount; page++)
+    {
         physical_address_t pAddr = memoryPhysicalAllocator.allocatePage();
         pagingMapPage(start + PAGE_SIZE * page, pAddr);
     }
 
-    logDebugn("%! Heap has been initialized.", "[Kernel Heap]");
+    logDebugn("%! Heap has been initialized. \
+                \n\t- Starting at: 0x%64x, \
+                \n\t- Ending At: 0x%64x", 
+                "[Kernel Heap]", start, end);
 }
 
-void *heapAllocate(uint64_t size) {
-    if (!heapInitialized) 
+void *heapAllocate(uint64_t size)
+{
+    if (!heapInitialized)
         kernelPanic("%! An attempt to allocate memory with uninitialized heap has occurred.", "[Kernel Heap]");
 
     // Allocate memory
     void *ptr = heapAllocator.allocate(size);
 
     // Handle the case where heap has ran out of memory
-    if (!ptr) {
-        if (heapExpand()) {
+    if (!ptr)
+    {
+        if (heapExpand())
+        {
             return heapAllocate(size);
         }
 
         kernelPanic("%! failed to allocate kernel memory.", "[Kernel Heap]");
         return 0;
-    }    
+    }
 
-    logDebugn("%! Successfully allocated %dKiB.", "[Kernel Heap]", size / 1024);
+    logDebugn("%! Successfully allocated %d %s.", "[Kernel Heap]", K_MEMORY_SIZE(size), K_MEMORY_UNIT(size));
     heapUsed += size;
     return ptr;
 }
 
-bool heapExpand() {
-    if (!heapInitialized) 
+bool heapExpand()
+{
+    if (!heapInitialized)
         kernelPanic("%! An attempt to expand an uninitialized heap has occurred.", "[Kernel Heap]");
 
-    if (heapEnd + K_HEAP_EXPANSION_STEP > K_CONST_HEAP_MAX_EXPANSION) {
+    if (heapEnd + K_HEAP_EXPANSION_STEP > K_CONST_HEAP_MAX_EXPANSION)
+    {
         logWarnn("%! Kernel heap has ran out of memory", "[Kernel Heap]");
         return false;
     }
@@ -64,40 +74,52 @@ bool heapExpand() {
     for (virtual_address_t virt = heapStart; virt < heapEnd + K_HEAP_EXPANSION_STEP; virt += PAGE_SIZE)
     {
         physical_address_t phys = memoryPhysicalAllocator.allocatePage();
-        if (phys == 0) {
+        if (phys == 0)
+        {
             logDebugn("%! Failed to expand heap, out of physical memory.", "[Kernel Heap]");
             return false;
         }
 
         pagingMapPage(virt, phys);
     }
-    
+
     heapAllocator.expand(K_HEAP_EXPANSION_STEP);
     heapEnd += K_HEAP_EXPANSION_STEP;
 
-    logDebugn("%! Heap has expanded to end %64x (%dKiB in use)", "[Kernel Heap]", heapEnd, heapUsed / 1024);
+    logDebugn("%! Heap has expanded by %d %s, up to 0x%64x (%d %s in use)",
+              "[Kernel Heap]",
+              K_MEMORY_SIZE(K_HEAP_EXPANSION_STEP),
+              K_MEMORY_UNIT(K_HEAP_EXPANSION_STEP),
+              heapEnd,
+              K_MEMORY_SIZE(heapUsed),
+              K_MEMORY_UNIT(heapUsed));
     return true;
 }
 
-void heapFree(void *mem) {
-    if (!heapInitialized) 
+void heapFree(void *mem)
+{
+    if (!heapInitialized)
         kernelPanic("%! An attempt to free memory with uninitialized heap has occurred.", "[Kernel Heap]");
 
     heapUsed -= heapAllocator.free(mem);
 }
 
-void *operator new (size_t count) {
+void *operator new(size_t count)
+{
     return heapAllocate(count);
 }
 
-void *operator new[] (size_t count) {
+void *operator new[](size_t count)
+{
     return heapAllocate(count);
 }
 
-void operator delete (void *ptr) {
+void operator delete(void *ptr)
+{
     heapFree(ptr);
 }
 
-void operator delete[] (void *ptr) {
+void operator delete[](void *ptr)
+{
     heapFree(ptr);
 }
