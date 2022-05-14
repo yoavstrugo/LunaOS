3f3a509fc183cb3fce817b4a9372932be09f384c
Yoav <yoav@strugo.net>
2022-05-14 22:47:43 +0300

    madt utilizing the structures after reading all

--- kernel/include/system/acpi/madt.hpp
+++ kernel/include/system/acpi/madt.hpp
@@ -2,61 +2,90 @@
 
 #include <system/acpi/acpi.hpp>
 
-struct k_madt_hdr {
+struct k_madt_hdr
+{
     k_acpi_sdt_hdr header;
     uint32_t localAPICAddr;
     uint32_t flags;
-}__attribute__((packed));
+} __attribute__((packed));
 
-enum MADT_ENTRY_TYPE {
-    P_LAPIC     = 0, // Processor Local APIC
-    IOAPIC      = 1, // I/O APIC
-    IOAPIC_ISO  = 2, // IO/APIC Interrupt Source Override
-    IOAPIC_NIS  = 3, // IO/APIC Non-maskable interrupt source
-    LAPIC_NI    = 4, // Local APIC Non-maskable interrupts
-    LAPIC_AO    = 5, // Local APIC Address Override
-    P_L2APIC    = 9  // Processor Local x2APIC
+enum MADT_ENTRY_TYPE
+{
+    P_LAPIC = 0,    // Processor Local APIC
+    IOAPIC = 1,     // I/O APIC
+    IOAPIC_ISO = 2, // IO/APIC Interrupt Source Override
+    IOAPIC_NIS = 3, // IO/APIC Non-maskable interrupt source
+    LAPIC_NI = 4,   // Local APIC Non-maskable interrupts
+    LAPIC_AO = 5,   // Local APIC Address Override
+    P_L2APIC = 9    // Processor Local x2APIC
 };
 
-struct k_madt_entry_header {
+template<class T>
+struct k_madt_entry_header_node {
+    T *header;
+    k_madt_entry_header_node<T> *next;
+};
+
+struct k_madt_entry_header
+{
     uint8_t entryType;
     uint8_t recordLength;
-}__attribute__((packed));
+} __attribute__((packed));
 
 // This type represents a single logical processor and its local interrupt controller.
-struct k_madt_lapic_entry {
+struct k_madt_lapic_entry
+{
     k_madt_entry_header header;
 
-    uint8_t     processorId; 
-    uint8_t     apicId;
-    uint32_t    flags; 
-}__attribute__((packed));
+    uint8_t processorId;
+    uint8_t apicId;
+    uint32_t flags;
+} __attribute__((packed));
 
-// This type represents a I/O APIC. 
-// The global system interrupt base is the first interrupt number that this I/O APIC handles.
-struct k_madt_ioapic_entry {
+/**
+ * @brief This optional structure supports 64-bit systems by
+ * providing an override of the physical address of the local
+ * APIC in the MADT’s table header, which is defined as a 32-bit field.
+ */
+struct k_madt_lapic_address_override_entry
+{
+    // Reserved (must be set to zero)
+    uint16_t reserved0;
+    // Physical address of Local APIC
+    uint64_t localAPICAddr;
+} __attribute__((packed));
+
+/**
+ * @brief This type represents a I/O APIC.
+ * The global system interrupt base is the first interrupt number that this I/O APIC handles.
+ */
+struct k_madt_ioapic_entry
+{
     k_madt_entry_header header;
 
-    uint8_t     ioapicId;
-    uint8_t     reserved0;
-    uint32_t    ioapicAddress;
-    uint32_t    globalSystemInterruptBase;
-}__attribute__((packed));
+    uint8_t ioapicId;
+    uint8_t reserved0;
+    uint32_t ioapicAddress;
+    uint32_t globalSystemInterruptBase;
+} __attribute__((packed));
 
-// This entry type contains the data for an Interrupt Source Override.
-// This explains how IRQ sources are mapped to global system interrupts
-struct k_madt_ioapic_interrupt_src_override_entry {
+/**
+ * @brief This entry type contains the data for an Interrupt Source Override.
+ * This explains how IRQ sources are mapped to global system interrupts
+ */
+struct k_madt_ioapic_interrupt_src_override_entry
+{
     k_madt_entry_header header;
 
-    uint8_t     busSource;
-    uint8_t     irqSource;
-    uint32_t    globalSystemInterrupt;
-    uint32_t    flags;
-}__attribute__((packed));
+    uint8_t busSource;
+    uint8_t irqSource;
+    uint32_t globalSystemInterrupt;
+    uint32_t flags;
+} __attribute__((packed));
 
 /**
  * @brief Parses the MADT table
- * 
+ *
  * @param madtHeader The madt header
  */
 void madtParse(k_acpi_sdt_hdr *madtHeader);
--- kernel/src/system/acpi/madt.cpp
+++ kernel/src/system/acpi/madt.cpp
@@ -1,15 +1,31 @@
 #include <system/acpi/madt.hpp>
 
 #include <logger/logger.hpp>
+#include <interrupts/lapic.hpp>
+#include <interrupts/ioapic.hpp>
+#include <stddef.h>
+#include <memory/heap.hpp>
+
+k_madt_entry_header_node<k_madt_lapic_entry> *lapicList = NULL;
+k_madt_entry_header_node<k_madt_ioapic_entry> *ioapicList = NULL;
+k_madt_entry_header_node<k_madt_ioapic_interrupt_src_override_entry> *ioapicInterruptSrcOverrideList = NULL;
 
 void madtParse(k_acpi_sdt_hdr *madtHeader)
 {
     k_madt_hdr *madt = (k_madt_hdr *)madtHeader;
 
+    physical_address_t lapicAddress = madt->localAPICAddr;
+
     uint8_t *madtData = (uint8_t *)((uint64_t)madt + sizeof(k_madt_hdr));
     uint32_t madtLength = madtHeader->length - sizeof(k_madt_hdr);
 
+    // Parse the table
     logDebugn("%! Table Entries:", "[MADT]");
+
+    #ifdef K_LOG_LEVEL_DEBUG
+    heapVerbose = false;
+    #endif
+
     uint32_t i = 0;
     while (i < madtLength)
     {
@@ -24,18 +40,56 @@ void madtParse(k_acpi_sdt_hdr *madtHeader)
                 logDebugn("\t- Found Processor Local APIC, ApicID: %d, ProcessorId: %d.",
                           entry->apicId,
                           entry->processorId);
+
+                // Add it to the linked list
+                k_madt_entry_header_node<k_madt_lapic_entry> *listEntry = new k_madt_entry_header_node<k_madt_lapic_entry>();
+                listEntry->next = lapicList;
+                listEntry->header = entry;
+                lapicList = listEntry;
+
                 break;
             }
         case IOAPIC:
             // TODO: handle ioapic
             {
-                k_madt_ioapic_entry *ioentry = (k_madt_ioapic_entry *)(entryHeader);
+                k_madt_ioapic_entry *entry = (k_madt_ioapic_entry *)(entryHeader);
                 logDebugn("\t- Found IO APIC, ApicID: %d, Address: 0x%64x, GSI Base: 0x%64x.",
-                          ioentry->ioapicId,
-                          ioentry->ioapicAddress,
-                          ioentry->globalSystemInterruptBase);
+                          entry->ioapicId,
+                          entry->ioapicAddress,
+                          entry->globalSystemInterruptBase);
+
+                // Add it to the linked list
+                k_madt_entry_header_node<k_madt_ioapic_entry> *listEntry = new k_madt_entry_header_node<k_madt_ioapic_entry>();
+                listEntry->next = ioapicList;
+                listEntry->header = entry;
+                ioapicList = listEntry;
+
                 break;
             }
+        case LAPIC_AO:
+        {
+            k_madt_lapic_address_override_entry *entry = (k_madt_lapic_address_override_entry *)(entryHeader);
+            logDebugn("\t- Found Local APIC Address Override, overriding lapic address to 0x%64x", entry->localAPICAddr);
+            break;
+        }
+        case IOAPIC_ISO:
+        {
+            k_madt_ioapic_interrupt_src_override_entry *entry = (k_madt_ioapic_interrupt_src_override_entry *)entryHeader;
+            logDebugn("\t- Found an IO/APIC Interrupt Source Override entry, \
+                            \n\t\t* Bus Source: %d \
+                            \n\t\t* IRQ Source: %d \
+                            \n\t\t* Global System Interrupt: %d",
+                      entry->busSource, entry->irqSource, entry->globalSystemInterrupt);
+
+            // Add it to the linked list
+            k_madt_entry_header_node<k_madt_ioapic_interrupt_src_override_entry> *listEntry =
+                new k_madt_entry_header_node<k_madt_ioapic_interrupt_src_override_entry>();
+            listEntry->next = ioapicInterruptSrcOverrideList;
+            listEntry->header = entry;
+            ioapicInterruptSrcOverrideList = listEntry;
+
+            break;
+        }
         default:
             logInfon("\t- Unsupported device type %d.", entryHeader->entryType);
             break;
@@ -43,4 +97,36 @@ void madtParse(k_acpi_sdt_hdr *madtHeader)
 
         i += entryHeader->recordLength;
     }
+
+    #ifdef K_LOG_LEVEL_DEBUG
+    heapVerbose = true;
+    #endif
+
+    // Handle the entries
+    // LAPIC
+    k_madt_entry_header_node<k_madt_lapic_entry> *lapicCurr = lapicList;
+    while (lapicCurr)
+    {
+        // TODO: handle it
+        lapicCurr = lapicCurr->next;
+    }
+
+    // IOAPIC
+    k_madt_entry_header_node<k_madt_ioapic_entry> *ioapicCurr = ioapicList;
+    while (ioapicCurr)
+    {
+        ioapicAdd(ioapicCurr->header->ioapicId, ioapicCurr->header->ioapicAddress, ioapicCurr->header->globalSystemInterruptBase);
+        ioapicCurr = ioapicCurr->next;
+    }
+
+    // IOAPIC Interrupt Source Override
+    k_madt_entry_header_node<k_madt_ioapic_interrupt_src_override_entry> *ioapicInterruptSrcOverrideCurr =
+        ioapicInterruptSrcOverrideList;
+    while (ioapicInterruptSrcOverrideCurr)
+    {
+        // TODO: handle it
+        ioapicInterruptSrcOverrideCurr = ioapicInterruptSrcOverrideCurr->next;
+    }
+
+    lapicPrepare(lapicAddress);
 }
