0e1e3744dbce0ecd654ab25b3a2b73299ad270b6
Yoav <yoav@strugo.net>
2022-05-14 23:28:42 +0300

    APIC timer

--- kernel/include/interrupts/requests.hpp
+++ kernel/include/interrupts/requests.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
 struct interrupt_frame;
+__attribute__((interrupt)) void requestTimer(interrupt_frame *frame);
 __attribute__((interrupt)) void requestKeyboardInt(interrupt_frame *frame);
--- kernel/src/interrupts/requests.cpp
+++ kernel/src/interrupts/requests.cpp
@@ -2,10 +2,24 @@
 
 #include <logger/logger.hpp>
 #include <interrupts/pic.hpp>
+#include <interrupts/lapic.hpp>
 #include <io.hpp>
 
 void requestKeyboardInt(interrupt_frame *frame) {
     logInfon("Pressed!");
     ioInByte(0x60);
-    picSendEOI(0x21);
+    lapicSendEOI();
+}
+
+uint64_t count = 0;
+uint64_t seconds = 0;
+
+void requestTimer(interrupt_frame *frame) {
+    count++;
+    if (count >= 1000) {
+        count = 0;
+        seconds++;
+        logDebugn("%! %ds", "[APIC Timer]", seconds);
+    }
+        lapicSendEOI();
 }
