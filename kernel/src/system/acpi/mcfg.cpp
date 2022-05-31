e0d2280deb6985e884af89314badd8e883cca3b5
Yoav Strugo <yoav@strugo.net>
2022-06-01 00:11:59 +0300

    no nned for this anymore

--- kernel/src/system/acpi/mcfg.cpp
+++ kernel/src/system/acpi/mcfg.cpp
@@ -4,20 +4,20 @@
 #include <memory/memory.hpp>
 #include <logger/logger.hpp>
 
-void mcfgParse(k_acpi_sdt_hdr *sdtHeader)
-{
-    k_mcfg_hdr *mcfgHeader = (k_mcfg_hdr *)sdtHeader;
+// void mcfgParse(k_acpi_sdt_hdr *sdtHeader)
+// {
+//     k_mcfg_hdr *mcfgHeader = (k_mcfg_hdr *)sdtHeader;
 
-    int entryCount = ((mcfgHeader->header.length) - sizeof(k_mcfg_hdr)) / sizeof(k_mcfg_entry);
+//     int entryCount = ((mcfgHeader->header.length) - sizeof(k_mcfg_hdr)) / sizeof(k_mcfg_entry);
 
-    for (int i = 0; i < entryCount; i++)
-    {
-        k_mcfg_entry *entry = (k_mcfg_entry *)((uint64_t)mcfgHeader + sizeof(k_mcfg_hdr) + i * sizeof(k_mcfg_entry));
-        for (uint8_t bus = entry->startBusNumber; bus < entry->endBusNumber; bus++)
-        {
-            pciParseBus(entry->baseAddress, bus - entry->startBusNumber);
-        }
-    }
+//     for (int i = 0; i < entryCount; i++)
+//     {
+//         k_mcfg_entry *entry = (k_mcfg_entry *)((uint64_t)mcfgHeader + sizeof(k_mcfg_hdr) + i * sizeof(k_mcfg_entry));
+//         for (uint8_t bus = entry->startBusNumber; bus < entry->endBusNumber; bus++)
+//         {
+//             pciParseBus(entry->baseAddress, bus - entry->startBusNumber);
+//         }
+//     }
 
-    logInfon("%! MCFG has been parsed", "[MCFG]");
-}
+//     logInfon("%! MCFG has been parsed", "[MCFG]");
+// }
